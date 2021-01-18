44bb7e571816aa282e9e46413c8c051701f1f230
Laika <laika@ForbiddenFruit.local>
2021-01-18 15:32:11 -0500

    added linting/prettier

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+};
--- day1/day1.ts
+++ day1/day1.ts
@@ -1,45 +1,43 @@
-const fs = require('fs');
+import { readFileSync } from 'fs';
+
 let data:string;
 
 try {
-     data = fs.readFileSync('./day1/input.txt', 'utf8');
-} catch(e) {
-    console.log('Error:', e.stack);
+  data = readFileSync('./day1/input.txt', 'utf8');
+} catch (e) {
+  console.log('Error:', e.stack);
 }
 
-let arrayedData:Array<string> = data.split("\n");
+const arrayedData:Array<string> = data.split('\n');
 arrayedData.pop();
-let numericArrayedData = arrayedData.map((e:string)=>parseInt(e));
+const numericArrayedData = arrayedData.map((e:string) => parseInt(e, 10));
 
-const totalAmount:number = 2020;
+const totalAmount = 2020;
 
-//prints day 1
-numericArrayedData.forEach((e:number,i:number )=>{
-    const searchValue:number = totalAmount - e;
-    const result:number = arrayedData.indexOf(searchValue.toString(),i+1); 
-    if(result > -1){
-        console.log(e*numericArrayedData[result])
+// prints day 1
+numericArrayedData.forEach((e:number, i:number) => {
+  const searchValue:number = totalAmount - e;
+  const result:number = arrayedData.indexOf(searchValue.toString(), i + 1);
+  if (result > -1) {
+    console.log(e * numericArrayedData[result]);
+  }
+});
+
+// part 2
+const findFactor = (currentNumericArray:Array<string>, targetValue:number,
+  totalFactors:number, currentResults:Array<number> = []) => {
+  if (currentResults.length >= totalFactors || currentNumericArray.length === 0) {
+    return;
+  }
+  currentNumericArray.forEach((e:string, i:number) => {
+    const elementAsNumber:number = parseInt(e, 10);
+    const searchValue:number = targetValue - elementAsNumber;
+    const holdingResults:Array<number> = [...currentResults, elementAsNumber];
+    if (searchValue === 0 && holdingResults.length === totalFactors) {
+      console.log(holdingResults.reduce((a, b) => a * b));
+    } else if (searchValue > 0 || currentNumericArray.length < totalFactors) {
+      findFactor(currentNumericArray.slice(i + 1), searchValue, totalFactors, holdingResults);
     }
-})
- 
-//part 2
-const findFactor = (currentNumericArray:Array<string>,targetValue:number,totalFactors:Number, currentResults:Array<number> = [] ) =>{
-    if(currentResults.length >= totalFactors || currentNumericArray.length === 0){
-        return;
-    }
-    currentNumericArray.forEach((e:string,i:number )=>{
-        const elementAsNumber:number = parseInt(e);
-        const searchValue:number = targetValue - elementAsNumber;
-        const holdingResults:Array<number> = [...currentResults,elementAsNumber];
-        if (searchValue === 0 && holdingResults.length === totalFactors){
-            console.log(holdingResults.reduce( (a, b) => a * b ));
-        }
-        else if(searchValue < 0 || currentNumericArray.length <=1){
-            return;
-        }
-        else{
-            findFactor(currentNumericArray.slice(i+1),searchValue,totalFactors,holdingResults);
-        }
-    })
-}
-findFactor(arrayedData,totalAmount,3);
+  });
+};
+findFactor(arrayedData, totalAmount, 3);
