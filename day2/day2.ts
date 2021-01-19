d9faa726956d868fce9d35a92398f993c915a8f9
LaikaFusion <laika@ForbiddenFruit.local>
2021-01-19 17:38:15 -0500

    day 2 part 2 done

--- day2/day2.ts
+++ day2/day2.ts
@@ -3,6 +3,7 @@ import * as inp from '../utils/inputer';
 const inputData = inp.breakOnLine(inp.getInput('./day2/input.txt'));
 
 let validCounter = 0;
+let validDay2Counter = 0;
 const regexForValidation = /(\d+)-(\d+)\s(\w+):\s(\w+)/;
 
 inputData.forEach((validationGroup:string) => {
@@ -11,6 +12,7 @@ inputData.forEach((validationGroup:string) => {
   const highestAllowed:number = parseInt(seperated[2], 10);
   const lowestAllowed:number = parseInt(seperated[1], 10);
   const checkLetter:string = seperated[3];
+  // day 1 stuff
   let checkCounter = 0;
 
   checkableArray.forEach((letter:string) => {
@@ -21,5 +23,12 @@ inputData.forEach((validationGroup:string) => {
   if (checkCounter <= highestAllowed && checkCounter >= lowestAllowed) {
     validCounter += 1;
   }
+
+  if ((checkableArray[lowestAllowed - 1] === checkLetter
+     && checkableArray[highestAllowed - 1] !== checkLetter)
+  || (checkableArray[lowestAllowed - 1] !== checkLetter
+    && checkableArray[highestAllowed - 1] === checkLetter)) {
+    validDay2Counter += 1;
+  }
 });
-console.log(validCounter);
+console.log(validCounter, validDay2Counter);
