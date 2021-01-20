2edaea4bfbf729e15fda3f4cd31d7a0c1d764075
LaikaFusion <laika@ForbiddenFruit.local>
2021-01-19 19:09:23 -0500

    day 3 part 2

--- day3/day3.ts
+++ day3/day3.ts
@@ -4,20 +4,32 @@ const inputData:Array<string> = inp.breakOnLine(inp.getInput('./day3/input.txt')
 const doubleSplitData:Array<Array<string>> = inputData.map((row:string) => row.split(''));
 
 const tree = '#';
-const vertDistance = 1;
-const horiDistance = 3;
-let horiCounter = 0;
-let treeCounter = 0;
 
-// part 1, the loop
-for (let index = 1; index < doubleSplitData.length; index += vertDistance) {
-  const element = doubleSplitData[index];
-  horiCounter += horiDistance;
-  if (horiCounter >= element.length) {
-    horiCounter -= element.length;
+const treeCounter = (vertDistance, horiDistance) => {
+  let horiCounter = 0;
+  let treeCounter = 0;
+
+  // part 1, the loop
+  for (let index = vertDistance; index < doubleSplitData.length; index += vertDistance) {
+    const element:Array<string> = doubleSplitData[index];
+    horiCounter += horiDistance;
+    if (horiCounter >= element.length) {
+      horiCounter -= element.length;
+    }
+    if (element[horiCounter] === tree) {
+      treeCounter += 1;
+    }
   }
-  if (element[horiCounter] === tree) {
-    treeCounter += 1;
-  }
-}
-console.log(treeCounter);
+  console.log(treeCounter);
+
+  return treeCounter;
+};
+
+const toFind:Array<Array<number>> = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]];
+let treeMultipleTotal = 1;
+// part 2 the loop
+toFind.forEach((pair:Array<number>) => {
+  const result = treeCounter(pair[0], pair[1]);
+  treeMultipleTotal *= result;
+});
+console.log(treeMultipleTotal);
