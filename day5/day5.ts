246164ff13d5de696ae67d782831dbf078707933
LaikaFusion <laika@ForbiddenFruit.local>
2021-01-19 22:01:03 -0500

    day 5 part 1

---
+++ day5/day5.ts
@@ -0,0 +1,20 @@
+import * as inp from '../utils/inputer';
+
+const inputData:Array<string> = inp.breakOnLine(inp.getInput('./day5/input.txt'));
+
+const holdingArr = [];
+
+// part 1 loop
+inputData.forEach((element) => {
+  let row = element.substr(0, 7);
+  let column = element.substr(7, 3);
+  row = row.replace(/F/g, '0').replace(/B/g, '1');
+  column = column.replace(/L/g, '0').replace(/R/g, '1');
+
+  const rowNum = parseInt(row, 2);
+  const columnNum = parseInt(column, 2);
+
+  const total = (rowNum * 8) + columnNum;
+  holdingArr.push(total);
+});
+console.log(Math.max(...holdingArr));
